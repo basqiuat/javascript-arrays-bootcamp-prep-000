06e3f803ee0ad5c4f7692b927da1758981657790
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-12-03 22:54:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -63,7 +63,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
   
 function removeElementFromEndOfArray(array){
-  array.slice(2, array.length - 2)
+  array.slice(0, array.length - 1)
   return array
 }
   
