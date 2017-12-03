747ea3ad460fcb93d222396e6ce24cb71a3212a3
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-12-03 21:23:36 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+ 
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+  ]
+  
+  addElementToBeginningOfArray function(){
+    
+  }  
+  
+  destructivelyAddElementToBeginningOfArray function
